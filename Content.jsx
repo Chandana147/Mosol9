5e105aaa5df45fe486a1454b65cc7da4fdeb8abe
Chaitanya Chandana Kakara <109449619+Chandana147@users.noreply.github.com>
2023-05-20 13:44:10 +0530

    Add files via upload

---
+++ Content.jsx
@@ -0,0 +1,49 @@
+import './Content.css'
+
+export const Content = () => {
+    return (
+        <div className="main">
+            <div className="size">
+                <div className="big">
+                    <div className="effect" >
+                        minimalist optimal
+                    </div>
+                    <div className="effect2">
+                        marketing
+                    </div>
+                </div>
+                <div className="matter">
+                    <div>
+                        <h5>Best Digital Marketing Agency in Hyderabad </h5>
+                    </div>
+                    <div>
+                        <h3>Mosol9 is a full-service digital marketing and technology company</h3>
+                        <h3>that's been purpose-built to help organizations thrive in an increasingly complex landscape. </h3>
+                    </div>
+                    <div><button className="dive">DIVE IN</button></div>
+                    <div className="symbols">
+                        <div className="strategy">
+                        <img className="diamond" src="https://thumbs.dreamstime.com/b/yellow-diamond-dark-background-illustration-design-logo-shapes-texture-creative-graphic-concept-new-art-jewelry-business-shop-164019968.jpg"/>
+                        <div>STRATEGY</div>
+                        <div>The blueprint of rough ideas and further actions</div>
+                         paves the way for successful campaign.
+                    </div>
+                    <div className="strategy">
+                        <img className="diamond" src="https://thumbs.dreamstime.com/b/yellow-diamond-dark-background-illustration-design-logo-shapes-texture-creative-graphic-concept-new-art-jewelry-business-shop-164019968.jpg"/>
+                        <div>STRATEGY</div>
+                        <div>The blueprint of rough ideas and further actions</div>
+                         paves the way for successful campaign.
+                    </div>
+                    <div className="strategy">
+                        <img className="diamond" src="https://thumbs.dreamstime.com/b/yellow-diamond-dark-background-illustration-design-logo-shapes-texture-creative-graphic-concept-new-art-jewelry-business-shop-164019968.jpg"/>
+                        <div>STRATEGY</div>
+                        <div>The blueprint of rough ideas and further actions</div>
+                         paves the way for successful campaign.
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Content;
